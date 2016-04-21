e6e55aaffec84344ce206c03f3ddeb579977a0ff
borko9696 <borko9696@mail.bg>
2016-04-21 14:53:40 +0300

    Menu design done , added some effects to buttons

--- WhoWantsToBeAMillionaireGAME/WhoWantsToBeAMillionaireGAME/Form1.Designer.cs
+++ WhoWantsToBeAMillionaireGAME/WhoWantsToBeAMillionaireGAME/Form1.Designer.cs
@@ -28,12 +28,193 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Logo = new System.Windows.Forms.PictureBox();
+            this.newGame1 = new System.Windows.Forms.PictureBox();
+            this.score1 = new System.Windows.Forms.PictureBox();
+            this.exit1 = new System.Windows.Forms.PictureBox();
+            this.newGame2 = new System.Windows.Forms.PictureBox();
+            this.score2 = new System.Windows.Forms.PictureBox();
+            this.exit2 = new System.Windows.Forms.PictureBox();
+            this.volumeOff = new System.Windows.Forms.PictureBox();
+            this.volumeOn = new System.Windows.Forms.PictureBox();
+            this.instruction = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.newGame1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.score1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exit1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.newGame2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.score2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exit2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.volumeOff)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.volumeOn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.instruction)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Logo
+            // 
+            this.Logo.BackColor = System.Drawing.Color.Transparent;
+            this.Logo.Image = ((System.Drawing.Image)(resources.GetObject("Logo.Image")));
+            this.Logo.Location = new System.Drawing.Point(245, 10);
+            this.Logo.Name = "Logo";
+            this.Logo.Size = new System.Drawing.Size(320, 210);
+            this.Logo.TabIndex = 6;
+            this.Logo.TabStop = false;
+            // 
+            // newGame1
+            // 
+            this.newGame1.BackColor = System.Drawing.Color.Transparent;
+            this.newGame1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.newGame1.Image = ((System.Drawing.Image)(resources.GetObject("newGame1.Image")));
+            this.newGame1.Location = new System.Drawing.Point(172, 226);
+            this.newGame1.Name = "newGame1";
+            this.newGame1.Size = new System.Drawing.Size(460, 68);
+            this.newGame1.TabIndex = 8;
+            this.newGame1.TabStop = false;
+            this.newGame1.MouseHover += new System.EventHandler(this.newGame1_MouseHover);
+            // 
+            // score1
+            // 
+            this.score1.BackColor = System.Drawing.Color.Transparent;
+            this.score1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.score1.Image = ((System.Drawing.Image)(resources.GetObject("score1.Image")));
+            this.score1.Location = new System.Drawing.Point(172, 290);
+            this.score1.Name = "score1";
+            this.score1.Size = new System.Drawing.Size(460, 68);
+            this.score1.TabIndex = 9;
+            this.score1.TabStop = false;
+            this.score1.MouseHover += new System.EventHandler(this.score1_MouseHover);
+            // 
+            // exit1
+            // 
+            this.exit1.BackColor = System.Drawing.Color.Transparent;
+            this.exit1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exit1.Image = ((System.Drawing.Image)(resources.GetObject("exit1.Image")));
+            this.exit1.Location = new System.Drawing.Point(172, 354);
+            this.exit1.Name = "exit1";
+            this.exit1.Size = new System.Drawing.Size(460, 68);
+            this.exit1.TabIndex = 10;
+            this.exit1.TabStop = false;
+            this.exit1.MouseHover += new System.EventHandler(this.exit1_MouseHover);
+            // 
+            // newGame2
+            // 
+            this.newGame2.BackColor = System.Drawing.Color.Transparent;
+            this.newGame2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.newGame2.Image = ((System.Drawing.Image)(resources.GetObject("newGame2.Image")));
+            this.newGame2.Location = new System.Drawing.Point(172, 226);
+            this.newGame2.Name = "newGame2";
+            this.newGame2.Size = new System.Drawing.Size(460, 68);
+            this.newGame2.TabIndex = 11;
+            this.newGame2.TabStop = false;
+            this.newGame2.MouseLeave += new System.EventHandler(this.newGame2_MouseLeave);
+            // 
+            // score2
+            // 
+            this.score2.BackColor = System.Drawing.Color.Transparent;
+            this.score2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.score2.Image = ((System.Drawing.Image)(resources.GetObject("score2.Image")));
+            this.score2.Location = new System.Drawing.Point(172, 290);
+            this.score2.Name = "score2";
+            this.score2.Size = new System.Drawing.Size(460, 68);
+            this.score2.TabIndex = 12;
+            this.score2.TabStop = false;
+            this.score2.Click += new System.EventHandler(this.pictureBox5_Click);
+            this.score2.MouseLeave += new System.EventHandler(this.score2_MouseLeave);
+            // 
+            // exit2
+            // 
+            this.exit2.BackColor = System.Drawing.Color.Transparent;
+            this.exit2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exit2.Image = ((System.Drawing.Image)(resources.GetObject("exit2.Image")));
+            this.exit2.Location = new System.Drawing.Point(172, 354);
+            this.exit2.Name = "exit2";
+            this.exit2.Size = new System.Drawing.Size(460, 68);
+            this.exit2.TabIndex = 13;
+            this.exit2.TabStop = false;
+            this.exit2.MouseLeave += new System.EventHandler(this.exit2_MouseLeave);
+            // 
+            // volumeOff
+            // 
+            this.volumeOff.BackColor = System.Drawing.Color.Transparent;
+            this.volumeOff.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.volumeOff.Image = ((System.Drawing.Image)(resources.GetObject("volumeOff.Image")));
+            this.volumeOff.Location = new System.Drawing.Point(12, 359);
+            this.volumeOff.Name = "volumeOff";
+            this.volumeOff.Size = new System.Drawing.Size(50, 50);
+            this.volumeOff.TabIndex = 14;
+            this.volumeOff.TabStop = false;
+            this.volumeOff.Click += new System.EventHandler(this.volumeOff_Click);
+            // 
+            // volumeOn
+            // 
+            this.volumeOn.BackColor = System.Drawing.Color.Transparent;
+            this.volumeOn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.volumeOn.Image = ((System.Drawing.Image)(resources.GetObject("volumeOn.Image")));
+            this.volumeOn.Location = new System.Drawing.Point(12, 359);
+            this.volumeOn.Name = "volumeOn";
+            this.volumeOn.Size = new System.Drawing.Size(50, 50);
+            this.volumeOn.TabIndex = 15;
+            this.volumeOn.TabStop = false;
+            this.volumeOn.Click += new System.EventHandler(this.volumeOn_Click);
+            // 
+            // instruction
+            // 
+            this.instruction.BackColor = System.Drawing.Color.Transparent;
+            this.instruction.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.instruction.Image = ((System.Drawing.Image)(resources.GetObject("instruction.Image")));
+            this.instruction.Location = new System.Drawing.Point(716, 332);
+            this.instruction.Name = "instruction";
+            this.instruction.Size = new System.Drawing.Size(68, 90);
+            this.instruction.TabIndex = 16;
+            this.instruction.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(784, 421);
+            this.Controls.Add(this.instruction);
+            this.Controls.Add(this.volumeOn);
+            this.Controls.Add(this.volumeOff);
+            this.Controls.Add(this.exit2);
+            this.Controls.Add(this.score2);
+            this.Controls.Add(this.newGame2);
+            this.Controls.Add(this.exit1);
+            this.Controls.Add(this.score1);
+            this.Controls.Add(this.newGame1);
+            this.Controls.Add(this.Logo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.newGame1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.score1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exit1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.newGame2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.score2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exit2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.volumeOff)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.volumeOn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.instruction)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Logo;
+        private System.Windows.Forms.PictureBox newGame1;
+        private System.Windows.Forms.PictureBox score1;
+        private System.Windows.Forms.PictureBox exit1;
+        private System.Windows.Forms.PictureBox newGame2;
+        private System.Windows.Forms.PictureBox score2;
+        private System.Windows.Forms.PictureBox exit2;
+        private System.Windows.Forms.PictureBox volumeOff;
+        private System.Windows.Forms.PictureBox volumeOn;
+        private System.Windows.Forms.PictureBox instruction;
     }
 }
 
--- WhoWantsToBeAMillionaireGAME/WhoWantsToBeAMillionaireGAME/Form1.cs
+++ WhoWantsToBeAMillionaireGAME/WhoWantsToBeAMillionaireGAME/Form1.cs
@@ -14,7 +14,74 @@ namespace WhoWantsToBeAMillionaireGAME
     {
         public Form1()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+   
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.newGame2.Visible = false;
+            this.score2.Visible = false;
+            this.exit2.Visible = false;
+            this.volumeOff.Visible = false;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newGame1_MouseHover(object sender, EventArgs e)
+        {
+            this.newGame1.Visible = false;
+            this.newGame2.Visible = true;
+        }
+
+        private void newGame2_MouseLeave(object sender, EventArgs e)
+        {
+            this.newGame1.Visible = true;
+            this.newGame2.Visible = false;
+        }
+
+        private void score1_MouseHover(object sender, EventArgs e)
+        {
+            this.score1.Visible = false;
+            this.score2.Visible = true;
+        }
+
+        private void score2_MouseLeave(object sender, EventArgs e)
+        {
+            this.score1.Visible = true;
+            this.score2.Visible = false;
+        }
+
+        private void exit1_MouseHover(object sender, EventArgs e)
+        {
+            this.exit1.Visible = false;
+            this.exit2.Visible = true;
+        }
+
+        private void exit2_MouseLeave(object sender, EventArgs e)
+        {
+            this.exit1.Visible = true;
+            this.exit2.Visible = false;
+        }
+
+        private void volumeOn_Click(object sender, EventArgs e)
+        {
+            this.volumeOn.Visible = false;
+            this.volumeOff.Visible = true;
+        }
+
+        private void volumeOff_Click(object sender, EventArgs e)
+        {
+            this.volumeOn.Visible = true;
+            this.volumeOff.Visible = false;
         }
     }
 }
